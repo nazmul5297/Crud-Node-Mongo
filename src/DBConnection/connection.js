a5ef9f10ec97c71e672efb6f87e46bc151775a53
nazmul5297 <nazmul.haque1@northsouth.edu>
2022-07-16 13:54:26 +0600

    daabase connected

--- src/DBConnection/connection.js
+++ src/DBConnection/connection.js
@@ -1,29 +1,37 @@
 var mongo = require("mongodb");
 var MongoClient = mongo.MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+// var url = "mongodb://localhost:27017/mydb";
+const config = require("../../config.json");
+const databaseConfig = config.database;
 
-MongoClient.connect(url, function (err, db) {
-  if (err) throw err;
-  var dbo = db.db("mydb");
-  var myobj = [
-    { name: "John", address: "Highway 71" },
-    { name: "Peter", address: "Lowstreet 4" },
-    { name: "Amy", address: "Apple st 652" },
-    { name: "Hannah", address: "Mountain 21" },
-    { name: "Michael", address: "Valley 345" },
-    { name: "Sandy", address: "Ocean blvd 2" },
-    { name: "Betty", address: "Green Grass 1" },
-    { name: "Richard", address: "Sky st 331" },
-    { name: "Susan", address: "One way 98" },
-    { name: "Vicky", address: "Yellow Garden 2" },
-    { name: "Ben", address: "Park Lane 38" },
-    { name: "William", address: "Central st 954" },
-    { name: "Chuck", address: "Main Road 989" },
-    { name: "Viola", address: "Sideway 1633" },
-  ];
-  dbo.collection("customers").insertMany(myobj, function (err, res) {
-    if (err) throw err;
-    console.log("Number of documents inserted: " + res.insertedCount);
-    db.close();
-  });
-});
+module.exports = class db {
+  constructor() {}
+  async connection() {
+    MongoClient.connect(databaseConfig.url, function (err, db) {
+      if (err) throw err;
+      console.log("database Connected");
+      // var dbo = db.db("mydb");
+      // var myobj = [
+      //   { name: "John", address: "Highway 71" },
+      //   { name: "Peter", address: "Lowstreet 4" },
+      //   { name: "Amy", address: "Apple st 652" },
+      //   { name: "Hannah", address: "Mountain 21" },
+      //   { name: "Michael", address: "Valley 345" },
+      //   { name: "Sandy", address: "Ocean blvd 2" },
+      //   { name: "Betty", address: "Green Grass 1" },
+      //   { name: "Richard", address: "Sky st 331" },
+      //   { name: "Susan", address: "One way 98" },
+      //   { name: "Vicky", address: "Yellow Garden 2" },
+      //   { name: "Ben", address: "Park Lane 38" },
+      //   { name: "William", address: "Central st 954" },
+      //   { name: "Chuck", address: "Main Road 989" },
+      //   { name: "Viola", address: "Sideway 1633" },
+      // ];
+      // dbo.collection("customers").insertMany(myobj, function (err, res) {
+      //   if (err) throw err;
+      //   console.log("Number of documents inserted: " + res.insertedCount);
+      //   db.close();
+      // });
+    });
+  }
+};
--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const crudModule = require("./crud");
+const database = require("./DBConnection/connection");
+const db = new database();
 
 const app = express();
 crudModule.init(app);
+db.connection();
 
 app.listen(3000, () => {
   console.log("Server Start at 3000");
